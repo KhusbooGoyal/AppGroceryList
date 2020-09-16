15c45bd40c03b0189b43a5760130cecc590de72c
Khusboo <khusboo1299@gmail.com>
2020-09-16 10:16:34 +0530

    App Grocery List

---
+++ app/src/main/java/com/example/mygrocerylist/Activities/ListActivity.java
@@ -0,0 +1,59 @@
+package com.example.mygrocerylist.Activities;
+
+import android.os.Bundle;
+
+import com.example.mygrocerylist.Data.DatabaseHandler;
+import com.example.mygrocerylist.Model.Grocery;
+import com.example.mygrocerylist.UI.RecyclerViewAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+
+import com.example.mygrocerylist.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter recyclerViewAdapter;
+    private List<Grocery> groceryList;
+    private List<Grocery> listItems;
+    private DatabaseHandler db;
+    //private Object RecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+            }
+        });
+
+        db = new DatabaseHandler(this);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerViewID);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        groceryList = new ArrayList<>();
+        listItems = new ArrayList<>();
+
+        //Get items from database
+        groceryList = db.getAllGroceries();
+
+    }
+}
